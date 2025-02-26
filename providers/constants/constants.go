4103077a3bb5650e2dcde4306830712f4c85ec55
Felipe Hernandez <fhernandez@beyondtrust.com>
2025-02-26 08:55:52 -0500

    fix: change fake URL to https://fakeurl:8080 in constants

--- providers/constants/constants.go
+++ providers/constants/constants.go
@@ -4,7 +4,7 @@ package constants
 
 const (
 	APIPath          = "/BeyondTrust/api/public/v3"
-	FakeApiUrl       = "https://localhost:8080" + APIPath
+	FakeApiUrl       = "https://fakeurl:8080" + APIPath
 	FakeClientId     = "faked050-e266-4b05-9ced-35e7dd5093ae"
 	FakeClientSecret = "fake3BMkkxe4OpdsJPMhmYTRSpeHJYA/NVmcnmPZv5s="
 )
