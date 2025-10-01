09dfccb13a62a5dd92ee3bf2ea149f96c26ee072
Felipe Hernandez <fhernandez@beyondtrust.com>
2025-10-01 14:30:06 -0500

    fix: solve linting issues

--- providers/provider_framework/provider.go
+++ providers/provider_framework/provider.go
@@ -145,20 +145,20 @@ func (p *PasswordSafeProvider) ValidateCredentialsAndConfig(ctx context.Context,
 	var errorSummary string
 
 	if data.apiKey == "" && data.clientId == "" && data.clientSecret == "" {
-		errorSummary = "Invalid Authentication method"
-		resp.Diagnostics.AddError("Invalid Authentication method", "Please add a valid credential (API Key / Client Credentials)")
+		errorSummary = "invalid authentication method"
+		resp.Diagnostics.AddError(fmt.Sprintf("Error: %s", errorSummary), "Please add a valid credential (API Key / Client Credentials)")
 		return errors.New(errorSummary)
 	}
 
 	if data.url == "" {
-		errorSummary = "Invalid URL"
-		resp.Diagnostics.AddError(errorSummary, "Please add a proper URL")
+		errorSummary = "invalid URL"
+		resp.Diagnostics.AddError(fmt.Sprintf("Error: %s", errorSummary), "Please add a proper URL")
 		return errors.New(errorSummary)
 	}
 
 	if data.apiKey != "" && data.accountname == "" {
-		errorSummary = "Invalid Account Name"
-		resp.Diagnostics.AddError(errorSummary, "Please add a proper Account Name")
+		errorSummary = "invalid account Name"
+		resp.Diagnostics.AddError(fmt.Sprintf("Error: %s", errorSummary), "Please add a proper Account Name")
 		return errors.New(errorSummary)
 	}
 	return nil
